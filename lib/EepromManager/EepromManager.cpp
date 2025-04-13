1af31b5fe508168cdb431e4a848a8c316ab5b932
Hendrik Putzek <hendrik.putzek@diva-e.com>
2025-04-13 23:23:14 +0200

    Add timePerMl to EEPROM management

--- lib/EepromManager/EepromManager.cpp
+++ lib/EepromManager/EepromManager.cpp
@@ -5,18 +5,19 @@ const int EepromManager::MAGIC_NUMBER;
 
 // Constructor
 EepromManager::EepromManager()
-    : stepsPerMl(0), hasLoadedFromEeprom(false)
+    : stepsPerMl(0), timePerMl(0), hasLoadedFromEeprom(false)
 {
 }
 
-// Save the stepsPerMl to EEPROM
+// Save the stepsPerMl and timePerMl to EEPROM
 void EepromManager::saveToEeprom() const
 {
     EEPROM.put(EEPROM_ADDR_MAGIC, MAGIC_NUMBER);
     EEPROM.put(EEPROM_ADDR_STEPS_PER_ML, stepsPerMl);
+    EEPROM.put(EEPROM_ADDR_TIME_PER_ML, timePerMl);
 }
 
-// Load the stepsPerMl from EEPROM
+// Load the stepsPerMl and timePerMl from EEPROM
 void EepromManager::loadFromEeprom()
 {
     int magic;
@@ -26,6 +27,7 @@ void EepromManager::loadFromEeprom()
     {
         // EEPROM is not initialized, set default values
         stepsPerMl = 0;
+        timePerMl = 0;
 
         // Save the default values to EEPROM
         saveToEeprom();
@@ -34,6 +36,7 @@ void EepromManager::loadFromEeprom()
     {
         // Load the saved value from EEPROM
         EEPROM.get(EEPROM_ADDR_STEPS_PER_ML, stepsPerMl);
+        EEPROM.get(EEPROM_ADDR_TIME_PER_ML, timePerMl);
         hasLoadedFromEeprom = true;
     }
 }
@@ -52,5 +55,22 @@ int EepromManager::getStepsPerMl()
 void EepromManager::setStepsPerMl(int steps)
 {
     stepsPerMl = steps;
+    saveToEeprom();
+}
+
+// Get the value of timePerMl
+int EepromManager::getTimePerMl()
+{
+    if (!hasLoadedFromEeprom)
+    {
+        loadFromEeprom();
+    }
+    return timePerMl;
+}
+
+// Set the value of timePerMl
+void EepromManager::setTimePerMl(int time)
+{
+    timePerMl = time;
     saveToEeprom();
 }
--- lib/EepromManager/EepromManager.h
+++ lib/EepromManager/EepromManager.h
@@ -1,22 +1,27 @@
-#ifndef EEPROM_MANAGER_H
-#define EEPROM_MANAGER_H
+#ifndef EepromManager_h
+#define EepromManager_h
 
-class EepromManager {
-private:
-    int stepsPerMl;
-    bool hasLoadedFromEeprom;
-
-    static const int EEPROM_ADDR_MAGIC = 0;
-    static const int EEPROM_ADDR_STEPS_PER_ML = 4;
-    static const int MAGIC_NUMBER = 0x1234;
-
+class EepromManager
+{
 public:
-    explicit EepromManager();
+    EepromManager();
     void saveToEeprom() const;
     void loadFromEeprom();
 
     int getStepsPerMl();
     void setStepsPerMl(int steps);
+    int getTimePerMl();
+    void setTimePerMl(int time);
+
+private:
+    static const int MAGIC_NUMBER = 0x1234; // Magic number to check if EEPROM is initialized
+    static const int EEPROM_ADDR_MAGIC = 0;
+    static const int EEPROM_ADDR_STEPS_PER_ML = 4;
+    static const int EEPROM_ADDR_TIME_PER_ML = 8;
+
+    int stepsPerMl;
+    int timePerMl;
+    bool hasLoadedFromEeprom;
 };
 
-#endif // EEPROM_MANAGER_H
+#endif
